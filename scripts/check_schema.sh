0ea69920b907487a4155cb733b62f9df5c0bea15
Anton Dukhovnikov <antond@wetafx.co.nz>
2025-11-25 08:53:37 +1300

    fix json validation
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- scripts/check_schema.sh
+++ scripts/check_schema.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+error=0
+
 for DIR in camera cmf illuminant training; do
     for FILE in ./data/$DIR/*; do
         echo "Processing file $FILE"
-        check-jsonschema --schemafile ./schema_0.1.0.json $FILE
+        if ! check-jsonschema --schemafile ./schema_0.1.0.json $FILE; then
+            error=1
+        fi
     done
 done
+
+exit $error
