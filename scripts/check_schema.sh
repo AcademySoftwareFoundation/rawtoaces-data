c945fdc7b360f7f1baf7cf257f5833a7bd7d302b
Anton Dukhovnikov <antond@wetafx.co.nz>
2025-11-25 13:52:23 +1300

    ensure the required properties are not empty
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- scripts/check_schema.sh
+++ scripts/check_schema.sh
@@ -5,7 +5,7 @@ error=0
 for DIR in camera cmf illuminant training; do
     for FILE in ./data/$DIR/*; do
         echo "Processing file $FILE"
-        if ! check-jsonschema --schemafile ./schema_0.1.0.json $FILE; then
+        if ! check-jsonschema --schemafile ./schema_1.0.0.json $FILE; then
             error=1
         fi
     done
